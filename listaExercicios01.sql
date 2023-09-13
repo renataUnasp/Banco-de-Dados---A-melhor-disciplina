56359bb868b77c5e9fef9214427d8d4c5698d44f
renataUnasp <144860138+renataUnasp@users.noreply.github.com>
2023-09-12 23:03:33 -0300

    listaExercicios01.sql

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -2,3 +2,6 @@ SELECT titulo FROM livros;
 
 SELECT nome FROM autores 
 WHERE nascimento < '1900-01-01';
+
+SELECT livros.titulo, nome.escritores FROM livros
+INNER JOIN escritores ON livros.escritor_id = escritores.id WHERE nome.escritores = 'J.K. Rowling';
