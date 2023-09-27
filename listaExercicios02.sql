cb4730d88f66ae8002c6a36aa8e120898228b380
renataUnasp <144860138+renataUnasp@users.noreply.github.com>
2023-09-26 23:12:48 -0300

    Update listaExercicios02.sql

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -10,3 +10,24 @@ END;
 
 DELIMITER ;
 CALL sp_ListarAutores();
+
+2 //
+
+DELIMITER //
+
+CREATE PROCEDURE sp_LivrosPorCategoria(IN Nome_Categoria VARCHAR(100))
+BEGIN
+SELECT Livro.Titulo
+FROM Livro
+INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID 
+WHERE Categoria.Nome = Nome_Categoria;
+END;
+//
+
+DELIMITER ;
+
+--atribuindo valores:
+CALL sp_LivrosPorCategoria('Romance');
+CALL sp_LivrosPorCategoria('Comedia');
+CALL sp_LivrosPorCategoria('Suspense');
+CALL sp_LivrosPorCategoria('Mistério');
