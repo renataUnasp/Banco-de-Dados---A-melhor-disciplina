51525a2d4810c49477a664d83c42c4e8ce587c5c
renataUnasp <144860138+renataUnasp@users.noreply.github.com>
2023-09-26 23:13:33 -0300

    Update listaExercicios02.sql

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -26,8 +26,31 @@ END;
 
 DELIMITER ;
 
+
 --atribuindo valores:
 CALL sp_LivrosPorCategoria('Romance');
 CALL sp_LivrosPorCategoria('Comedia');
 CALL sp_LivrosPorCategoria('Suspense');
 CALL sp_LivrosPorCategoria('Mistério');
+
+3 //
+
+DELIMITER //
+
+CREATE PROCEDURE sp_ContarLivrosPorCategoria(IN Nome_Categoria VARCHAR(200), OUT Total_Livros INT)
+BEGIN
+SELECT COUNT(*) INTO Total_Livros
+FROM Livro
+INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+WHERE Categoria.Nome = Nome_Categoria;
+END;
+//
+
+DELIMITER ;
+
+--atribuindo valores:
+CALL sp_ContarLivrosPorCategoria('Romance', @total_livros);
+CALL sp_ContarLivrosPorCategoria('Ciência', @total_livros);
+CALL sp_ContarLivrosPorCategoria('Ficção Científica', @total_livros);
+CALL sp_ContarLivrosPorCategoria('História', @total_livros);
+CALL sp_ContarLivrosPorCategoria('Autoajuda', @total_livros);
